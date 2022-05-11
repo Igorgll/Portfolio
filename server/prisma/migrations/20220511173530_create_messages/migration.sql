e6d88d1c19584c1e84ffe4631443b337cceabba5
Igor Lima <igorlimagn@gmail.com>
2022-05-11 15:40:42 -0300

    Server nodeJs

---
+++ server/prisma/migrations/20220511173530_create_messages/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "text" TEXT NOT NULL
+);
